67d212bc26804e511e5e2f3a3d66705708e2d45a
Frank Hu <tianyaoh@hotmail.com>
2023-09-22 22:36:47 +0800

    Ditching difflib; Switched to fuzzywuzzy; Add Fix to BOs

--- clean_vid_csv.py
+++ clean_vid_csv.py
@@ -1,16 +1,32 @@
 import pandas as pd
 import numpy as np
 import difflib
+from fuzzywuzzy import process
 import re
 import logging
 
+#Constants
+path_analysis = "E:\dev\Python\CS_Twitch\\video_analysis.csv"
+path_info = "E:\dev\Python\CS_Twitch\\basic_information.csv"
 
-def GetMostSimilar(base, ls_guess, guess_up = False, guess_low = False, n = 1, cutoff = 0.1):
+#Configure log file
+log_name = path_analysis[path_analysis.rindex('\\')+1:-4] + ".log"
+logging.basicConfig(filename=log_name, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.FileHandler(log_name, mode='w')
+
+df_analysis = pd.read_csv(path_analysis)
+df_info = pd.read_csv(path_info)
+
+#First Step, Ensure game view
+df_analysis = df_analysis.loc[df_analysis["Stage"].notnull()]
+
+def GetMostSimilar(base, ls_guess, guess_up = False, guess_low = False, n = 1, cutoff = 0.01):
+    ls_guess = list(ls_guess)
     if guess_up:
         ls_guess = [x.upper() for x in ls_guess]
     elif guess_low:
         ls_guess = [x.lower() for x in ls_guess]
-    return difflib.get_close_matches(base, ls_guess, n = n, cutoff=cutoff)[0]
+    return process.extractOne(base, ls_guess)[0]
 
 #Fix map name
 #Create a mapping from original name to the mostlikely name
@@ -22,19 +38,15 @@ def createDictGuess(ls_base_name, ls_target_name):
     return dict_truth
 
 #To fix certain column with an iterable(usually ) ground truth
-def fix_col_with_replace(ls_col_id, ls_truth, df_target):
+def fix_col_with_replace(ls_col_id, ls_truth, df_target = df_analysis):
     #Collect all the columns that needs to be fixed
     #   - assume that they are in the same domain
     #       -e.g play1_name, player2_name are within the same domain; map and hp are not in the same domain
     ls_bad = set()
-    i = 0
     for column_name in ls_col_id:
-        print("i is ", i)
-        print(set(df_target[column_name]))
         ls_bad = ls_bad|set(df_target[column_name])
 
     dict_guess = createDictGuess(ls_bad, ls_truth)
-    i = 0
     for key in dict_guess.keys():
         for columnName in ls_col_id:
             df_target[columnName] = df_target[columnName].replace(key, dict_guess[key])
@@ -51,12 +63,6 @@ log_name = path_analysis[path_analysis.rindex('\\')+1:-4] + ".log"
 logging.basicConfig(filename=log_name, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.FileHandler(log_name, mode='w')
 
-# logging.debug('This is a debug message')
-# logging.info('This is an info message')
-# logging.warning('This is a warning message')
-# logging.error('This is an error message')
-# logging.critical('This is a critical message')
-
 df_analysis = pd.read_csv(path_analysis)
 df_info = pd.read_csv(path_info)
 
@@ -66,11 +72,14 @@ df_analysis = df_analysis.loc[df_analysis["Stage"].notnull()]
 #FIX MAP NAMES:
 #All csgo maps
 ls_map_name = ["inferno", 'mirage', 'nuke', 'overpass', 'vertigo', 'ancient', 'anubis', 'dust ii', 'train', 'cache']
-fix_col_with_replace(["Map"], ls_map_name, df_target = df_analysis)
+fix_col_with_replace(["Map"], ls_map_name)
 
 #FIX TEAM NAMES
 ls_team_name =  list(df_info['Team'])
-fix_col_with_replace(["Team_0", "Team_1"], ls_team_name, df_target = df_analysis)
+fix_col_with_replace(["Team_0", "Team_1"], ls_team_name)
+
+ls_BO = ["BO1", "BO3"]
+fix_col_with_replace(["BO"], ls_BO)
 
 #print(df_analysis)
 
@@ -92,18 +101,21 @@ def _stage(text):
     ls_group_stage = set(df_info['From'])
     #strip white space
     text = text.strip()
-    pattern = r'(\S+\s?)([\d|o|O]\s?\W?\s?[\d|o|O])'
+    pattern = r'(\S+\s?)([\d|o|O]\W?\s?[\d|o|O])'
     match = re.search(pattern, str(text))
     try:
         return GetMostSimilar(match.group(1), ls_group_stage) + " "+ match.group(2)
     except:
         logging.error("<STAGE> Unable to convert \'" + text + "\' to appropriate format like \'LEGEND 0-0\'")
 
+def _hp(text):
+    pattern = r'\\p{P}'
+    no_punc = re.sub(pattern, '', text)
+    return no_punc
+
 # #use regex fix time
 fix_col_with_fun(['Stage'], _stage)
-fix_col_with_fun(['Ingame_Time_Left'], _time)
+# fix_col_with_fun(['Ingame_Time_Left'], _time)
+fix_col_with_replace(['Player_HP_'+str(i) for i in range(10)], [str(x) for x in list(range(101))])
 
-
-print(df_analysis.head(29))
-
-
+print(df_analysis['BO'].head(10))

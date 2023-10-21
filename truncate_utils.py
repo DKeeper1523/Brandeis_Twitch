beca9af03d0fde6f2a24e503d47a0b4a9f35c546
tianyaoh <65034293+tianyaoh@users.noreply.github.com>
2023-10-21 16:51:34 +0800

    renamed functions in truncate_utils

--- truncate_utils.py
+++ truncate_utils.py
@@ -1,42 +1,35 @@
 import pandas as pd
 import numpy as np
-from math import ceil
-import sys
+import logging
 
-np.set_printoptions(threshold=sys.maxsize)
-
-def truncateAll(df_text, df_audio, df_video):
-    print("reached truncate all")
+def truncateAndCombineAll(df_text, df_audio, df_video, file_name):
+    truncateAudio(df_audio)
+    truncateVideo(df_video)
     len_text = len(df_text)
-    truncateAudio(df_audio, len_text)
-    truncateVideo(df_video, len_text)
-    print("text:", len(df_text), "audio", len(df_audio), "video", len(df_video))
-    print("\n\n\n\n\n")
+    len_video = len(df_video)
+    len_audio = len(df_audio)
+    min_len = min([len_audio, len_text, len_video])
+    #drop old indicies
+    df_audio = df_audio.iloc[:min_len, 1:]
+    df_video = df_video.iloc[:min_len, 1:]
+    df_text = df_text.iloc[:min_len, 1:]
+    # print("text:", len(df_text), "audio", len(df_audio), "video", len(df_video))
+    #concatenate
+    combined = pd.concat([df_video, df_audio, df_text])
+    return combined
 
-def truncateAudio(df_audio, len_text, num_split = 5):
-    diff = len(df_audio) - len_text
-    #skipping if it is shorter than text csv
-    if diff <= 0:
-        pass
-    else:
-        audio_step = (len(df_audio) - diff)//num_split
-        #stepping for inex
-        FLAG_INDEX = [1,3] #deleting on 2nd and 4th place
-        flag = 0
-        print("Audio drop: ", diff) 
-        #should be log message here
-        for i in range(audio_step, len(df_audio), audio_step):
-            if flag in FLAG_INDEX:
-                print(flag)
-                # df_audio.drop(i, inplace = True)
-            flag += 1
-        print("truncateAudio Finished")
+def truncateAudio(df_audio, num_split = 5):
+    audio_step = len(df_audio)//num_split
+    #stepping for inex
+    FLAG_INDEX = [1,3] #deleting on 2nd and 4th place
+    flag = 0
+    #should be log message here
+    for i in range(audio_step, len(df_audio), audio_step):
+        if flag in FLAG_INDEX:
+            logging.info("Audio dropping row#: "+str(i))
+            df_audio.drop(i, inplace=True)
+        flag += 1
 
-def truncateVideo(df_video, len_text, step = 1000):
-    diff = len(df_video) - len_text
-    print("reached truncated Video", diff)
-    if diff <= 0:
-        pass
-    else:
-        indicies2drop = range(step, len(df_video), step)
-        print("Video Drop:\n", list(indicies2drop))
+def truncateVideo(df_video, step = 1000):
+    indicies2drop = range(step-1, len(df_video), step)
+    df_video.drop(pd.Index(indicies2drop), axis=0, inplace=True)

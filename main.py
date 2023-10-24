ed63f26ec21bceccaacd5d402f2c7ee7335f0a24
tianyaoh <65034293+tianyaoh@users.noreply.github.com>
2023-10-24 21:46:51 +0800

    fixed merge overflow/missing argument

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(dest='src', type=str, help="Looks under the current working directory for the directory containing raw video analysis data (it should have folders for each stream)")
 parser.add_argument(dest='path_csv_info', type=str, help="CSV containing basic infommation for team competing")
 parser.add_argument('-out', type=str, help="directory for storing cleaned video data")
-# parser.add_argument(dest='info', type=str, help="Directory containing video analysis folder")
+parser.add_argument('-l', '--log', action='store_true', help='Toggle logging')
 
 #Run with: python Brandeis_Twitch_RA/main.py rawdata Brandeis_Twitch_RA/basic_information.csv -out clean
 
@@ -58,7 +58,8 @@ if __name__ == "__main__":
         path_out = dir_out + path2data[path2data.rindex('/'):]
 
         #init logger
-        init_log(log_name = path_out + ".log")
+        if args.log:
+            init_log(log_name = path_out + ".log")
 
         #reading
         df_text = pd.read_csv(path2data + CSV_TEXT)
@@ -66,12 +67,13 @@ if __name__ == "__main__":
         df_audio = pd.read_csv(path2data + CSV_AUDIO)
 
         #clean data
-        df_video = cleanVideoDf(df_video, df_info) 
+        # df_video = cleanVideoDf(df_video, df_info) 
 
-        all = truncateAndCombineAll(df_text, df_audio, df_video, path2data)
+        all = truncateAndCombineAll(df_text, df_audio, df_video)
         #write to csv
-        all.to_csv(path_out + ".csv", index = True, index_label="Time_Stamp")
+        if all is not None:
+            all.to_csv(path_out + ".csv", index = True, index_label="Time_Stamp")
         # df_video.to_csv(path_out + ".csv", index = False)
 
         print(path2data, "finished")
-        # break
+        break
--- truncate_utils.py
+++ truncate_utils.py
@@ -3,24 +3,21 @@ import numpy as np
 from utils import printFull
 import logging
 
-def truncateAndCombineAll(df_text, df_audio, df_video, file_name):
+def truncateAndCombineAll(df_text, df_audio, df_video):
     truncateAudio(df_audio)
     truncateVideo(df_video)
-    len_text = len(df_text)
-    len_video = len(df_video)
-    len_audio = len(df_audio)
-    min_len = min([len_audio, len_text, len_video])
-    #drop old indicies
-    df_audio = df_audio.iloc[:min_len, 1:]
-    df_video = df_video.iloc[:min_len, 1:]
-    df_text = df_text.iloc[:min_len, 1:]
+    #get the minimal length
+    min_len = min([len(x) for x in [df_video, df_audio, df_text]])
 
-    print("text:", len(df_text), "audio", len(df_audio), "video", len(df_video), "min len", min_len)
+    df_audio = df_audio.iloc[:, 1:]
+    df_video = df_video.iloc[:, 1:]
+    df_text = df_text.iloc[:, 1:]
+
     #concatenate
     combined = pd.concat([df_video, df_audio, df_text], axis=1)
-
-    print("len(combined)", len(combined))
-    return combined.iloc[:min_len:, :] 
+    #trucate to the shortest df
+    combined = combined.iloc[:min_len, :]
+    return combined
 
 def truncateAudio(df_audio, num_split = 5):
     audio_step = len(df_audio)//num_split
@@ -31,7 +28,7 @@ def truncateAudio(df_audio, num_split = 5):
     for i in range(audio_step, len(df_audio), audio_step):
         if flag in FLAG_INDEX:
             logging.info("Audio dropping row#: "+str(i))
-            df_audio = df_audio.drop(i)
+            df_audio.drop(i, axis = 0, inplace = True)
         flag += 1
 
 def truncateVideo(df_video, step = 1000):

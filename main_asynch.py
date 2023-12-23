c759bdbbd24bbe9b5d72c32a18ff43481bdd6708
tianyaoh <65034293+tianyaoh@users.noreply.github.com>
2023-12-22 22:12:21 -0500

    attempting to counter utf-8 error

--- main_asynch.py
+++ main_asynch.py
@@ -80,7 +80,7 @@ def cleanAndMerge(name_sub):
 
     #reading
     df_text = pd.read_csv(path2data + CSV_TEXT)
-    df_video = pd.read_csv(path2data + CSV_VIDEO, low_memory=False)
+    df_video = pd.read_csv(path2data + CSV_VIDEO, low_memory=False, encoding='latin1')
     df_audio = pd.read_csv(path2data + CSV_AUDIO)
 
 

7b9c72cde332ada8c823225eed13813e9cc6fce8
DKeeper1523 <353295986@qq.com>
2025-06-03 20:46:40 -0400

    remove df_vocal and df_music

--- main_asynch.py
+++ main_asynch.py
@@ -67,8 +67,8 @@ def cleanAndMerge(name_sub):
     CSV_TEXT = "/audio_text_analysis.csv"
     CSV_VIDEO = "/video_analysis.csv"
     CSV_AUDIO = "/audio_analysis.csv"
-    CSV_VOCAL = "/audio_analysis_vocal.csv"
-    CSV_MUSIC = "/audio_analysis_accompaniment.csv"
+    # CSV_VOCAL = "/audio_analysis_vocal.csv"
+    # CSV_MUSIC = "/audio_analysis_accompaniment.csv"
 
     path2data = dir_src + '/' + name_sub
     path_out = dir_members + '/' + name_sub
@@ -83,8 +83,8 @@ def cleanAndMerge(name_sub):
     df_text = pd.read_csv(path2data + CSV_TEXT)
     df_video = pd.read_csv(path2data + CSV_VIDEO, low_memory=False, encoding='latin1')
     df_audio = pd.read_csv(path2data + CSV_AUDIO)
-    df_vocal = pd.read_csv(path2data + CSV_VOCAL)
-    df_music = pd.read_csv(path2data + CSV_MUSIC)
+    # df_vocal = pd.read_csv(path2data + CSV_VOCAL)
+    # df_music = pd.read_csv(path2data + CSV_MUSIC)
 
     cur_process_id = current_process()._identity[0]-1
     #clean data
@@ -94,7 +94,7 @@ def cleanAndMerge(name_sub):
     unify_index(df_video, [df_text, df_audio])
 
     #combine combined
-    combined = truncateAndCombineAll(df_text, df_audio, df_video, df_vocal, df_music)
+    combined = truncateAndCombineAll(df_text, df_audio, df_video)
 
     #reset and insert index
     combined.reset_index(drop=True, inplace=True)
